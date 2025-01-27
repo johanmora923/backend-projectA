2f06884cff045409c95f08d082e8287bbc7abe02
johanmora923 <johanmora923@gmail.com>
2025-01-26 21:07:10 -0400

    init

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+
+const app = express();
+app.set("port",4000)
+app.listen(app.get("port"))
+console.log("Server on port", app.get("port"));
+
+app.use(express.json());
+app.use(express.static(__dirname + '/pages'));
+
+
+app.get('/', (req, res) => { res.sendFile(__dirname + '/public/index.html'); });
