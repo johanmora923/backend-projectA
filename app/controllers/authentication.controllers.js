a7794fb0b44c6c68212e656091c469fc3459065d
johanmora923 <johanmora923@gmail.com>
2025-01-28 13:30:16 -0400

    update

--- app/controllers/authentication.controllers.js
+++ app/controllers/authentication.controllers.js
@@ -1,29 +1,29 @@
 import { createPool } from 'mysql2/promise';
 
-try{
-const pool = createPool({
-    host: 'bwvxcku5txdwk7meiayz-mysql.services.clever-cloud.com',
-    user: 'u1xrecofby3ual5y',
-    password: 'ZgoqH9tZgXX1XfZroUUj',
-    database: 'bwvxcku5txdwk7meiayz',
-    connectionLimit: 5,
-    port: 3306
-});
-}
-catch(error){
+let pool;
+try {
+    pool = createPool({
+        host: 'bwvxcku5txdwk7meiayz-mysql.services.clever-cloud.com',
+        user: 'u1xrecofby3ual5y',
+        password: 'ZgoqH9tZgXX1XfZroUUj',
+        database: 'bwvxcku5txdwk7meiayz',
+        connectionLimit: 5,
+        port: 3306
+    });
+} catch (error) {
     console.log('Error en la conexion a la base de datos', error);
-}   
+}
 
-async function handleLogin(req, res) {
+async function handleLogin( req, res ) {
+    const { user, password } = req.body;
+    if (!user || !password) {
+        return res.status(400).send('User and password are required');
+    }
     try{
-        const { user, password } = req.body;
-        const { users } = await pool.query('SELECT * FROM users');
+        const [ users ] = await pool.query('SELECT name,password FROM users WHERE name = ?', [user]);
         for (let userDB of users) {
             console.log(userDB);
-            if (!user || !password) {
-                return res.status(400).send('User and password are required');
-            }
-            else if (user !== userDB.name  || password !== userDB.password) {
+            if (user !== userDB.name  || password !== userDB.password) {
                 console.log('problema de inicio de sesion');
                 return res.status(401).send('User or password are incorrect');
             }
