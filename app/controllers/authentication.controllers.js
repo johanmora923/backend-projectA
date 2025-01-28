b22fa3ecb9844990a2bf145e977b81dbaea18889
johanmora923 <johanmora923@gmail.com>
2025-01-28 12:35:28 -0400

    pool conection

--- app/controllers/authentication.controllers.js
+++ app/controllers/authentication.controllers.js
@@ -1,20 +1,35 @@
+import createPool from 'mysql2/promise';
+
+const pool = createPool.createPool({
+    host: 'bwvxcku5txdwk7meiayz-mysql.services.clever-cloud.com',
+    user: 'u1xrecofby3ual5y',
+    password: 'ZgoqH9tZgXX1XfZroUUj',
+    database: 'bwvxcku5txdwk7meiayz',
+    connectionLimit: 5,
+    port: 3306
+});
 
 
 async function handleLogin(req, res) {
-    const { user, password } = req.body;
-    const users = {
-        user: 'johan',
-        password: '1234'
+    try{
+        const { user, password } = req.body;
+        const { users } = await pool.query('SELECT * FROM users');
+        for (let userDB of users) {
+            console.log(userDB);
+            if (!user || !password) {
+                return res.status(400).send('User and password are required');
+            }
+            else if (user !== userDB.name  || password !== userDB.password) {
+                console.log('problema de inicio de sesion');
+                return res.status(401).send('User or password are incorrect');
+            }
+            console.log('inicio de sesion exitoso');
+            return res.status(200).send('Login successful');
+        }
     }
-    if (!user || !password) {
-        return res.status(400).send('User and password are required');
+    catch(error){
+        return res.status(500).send('Internal server error', error);  
     }
-    else if (user !== users.user || password !== users.password) {
-    console.log('problema de inicio de sesion');
-    return res.status(401).send('User or password are incorrect');
-    }
-    console.log('inicio de sesion exitoso');
-    return res.status(200).send('Login successful');
 }
 
 export const METHODS = {
